8619561351053545b9fb72fe67539c182a7ac646
stark042004 <stark042004@yandex.ru>
2021-09-03 14:31:57 +0300

    changed favicons

--- lib/Screens/Store/store_screen.dart
+++ lib/Screens/Store/store_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:interview_practicing/Screens/common_components/background_image.dart';
 import 'package:interview_practicing/constants.dart';
 import 'package:interview_practicing/Screens/common_components/app_bar.dart';
-import 'package:expansion_card/expansion_card.dart';
 import 'package:interview_practicing/models/Company.dart';
 import 'components/company_card.dart';
 
